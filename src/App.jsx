b0d827cd01c8d1fe827ca620255243a3ed7d1ddd
ramnargave <ramnargave18@gmail.com>
2025-01-08 18:02:15 +0530

    form

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 import './App.css'
+import Form from './components/ram/form'
 
 function App() {
   return (
      <>
-     <div>
-      <form action="">
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" name="name" placeholder='Enter your Name' required />
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" placeholder='Enter your Email' required />
-      </form>
-     </div>
+    <Form></Form>
      </>
   )
 }
---
+++ src/components/ram/Form.jsx
@@ -0,0 +1,18 @@
+
+
+function Form() {
+  return (
+    <>
+     <div>
+      <form action="">
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" name="name" placeholder='Enter your Name' required />
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" placeholder='Enter your Email' required />
+      </form>
+     </div>
+    </>
+  )
+}
+
+export default Form
